680c444c62486ac988faea0f63ecb6b64e115cc8
Musharraf2000 <musharraf.azhar19@gmail.com>
2026-04-02 08:51:42 +0530

    Created a list of book objects

---
+++ Week4COM25A1/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4COM25A1
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+    }
+}
--- Week4COM25A1/Program.cs
+++ Week4COM25A1/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//List<int> numbers = new List<int>([2,4,6,7,8]);
+
+//foreach(int number in numbers)
+//{
+//    Console.WriteLine(number);
+//}
+
+using Week4COM25A1;
+
+Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+Book book3 = new Book("1984", "George Orwell");
+
+
+List<Book> booksIntheLibrary = new List<Book>([book1,book2,book3]);
+
+foreach (Book book in booksIntheLibrary)
+{
+    Console.WriteLine(book.Title);
+}
